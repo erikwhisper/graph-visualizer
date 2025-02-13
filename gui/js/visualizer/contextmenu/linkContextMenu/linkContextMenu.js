f3cea747eecde1bff79fb57116dca5992ca6c9ae
erikwhisper <erlud100@uni-duesseldorf.de>
2025-02-13 16:41:25 +0100

    refactored link-context-menu

--- gui/js/visualizer/contextmenu/linkContextMenu/linkContextMenu.js
+++ gui/js/visualizer/contextmenu/linkContextMenu/linkContextMenu.js
@@ -8,230 +8,138 @@ function linkContextMenu(svg) {
     "data-link-id",
     (d) => d.linkId // Unique linkId
   );
-  implementLinksContextMenu();
+  setupLinkContextMenuInteractions();
   closeContextMenu("link-context-menu");
 }
 
-function implementLinksContextMenu() {
-  console.log("Link Contextmenu called");
+function setupLinkContextMenuInteractions() {
+  document.getElementById("arrowhead-normal").addEventListener("click", () => updateArrowhead("normal"));
+  document.getElementById("arrowhead-odot").addEventListener("click", () => updateArrowhead("odot"));
+  document.getElementById("arrowhead-tail").addEventListener("click", () => updateArrowhead("tail"));
 
-  function handleArrowheadClick(arrowheadType) {
-    const linkMenu = document.getElementById("link-context-menu");
-    const linkId = linkMenu.getAttribute("data-link-id");
-    const selectedLink = jsonData.links.find((link) => link.linkId === linkId);
+  document.getElementById("arrowtail-normal").addEventListener("click", () => updateArrowtail("normal"));
+  document.getElementById("arrowtail-odot").addEventListener("click", () => updateArrowtail("odot"));
+  document.getElementById("arrowtail-tail").addEventListener("click", () => updateArrowtail("tail"));
 
-    if (selectedLink) {
-      console.log(
-        "hier weiß er was der arrowhead ist: " + selectedLink.arrowhead
-      );
-      selectedLink.arrowhead = arrowheadType;
-
-      const markerId = `marker-${selectedLink.linkId}-end`;
-      setupArrowMarker(
-        d3.select("svg"),
-        markerId,
-        arrowheadType,
-        selectedLink.linkColor,
-        "auto"
-      );
-
-      d3.select(`#link-${selectedLink.linkId}`).attr(
-        "marker-end",
-        `url(#${markerId})`
-      );
-
-      updatePagJsonDisplay();
-      console.log(`Arrowhead updated to '${arrowheadType}' for link ${linkId}`);
-    }
-  }
-
-  document
-    .getElementById("arrowhead-normal")
-    .addEventListener("click", () => handleArrowheadClick("normal"));
-  document
-    .getElementById("arrowhead-odot")
-    .addEventListener("click", () => handleArrowheadClick("odot"));
-  document
-    .getElementById("arrowhead-tail")
-    .addEventListener("click", () => handleArrowheadClick("tail"));
-
-  function handleArrowtailClick(arrowtailType) {
-    const linkMenu = document.getElementById("link-context-menu");
-    const linkId = linkMenu.getAttribute("data-link-id");
-    const selectedLink = jsonData.links.find((link) => link.linkId === linkId);
-
-    if (selectedLink) {
-      selectedLink.arrowtail = arrowtailType;
-
-      const markerId = `marker-${selectedLink.linkId}-start`;
-      setupArrowMarker(
-        d3.select("svg"),
-        markerId,
-        arrowtailType,
-        selectedLink.linkColor,
-        "auto-start-reverse"
-      );
-
-      d3.select(`#link-${selectedLink.linkId}`).attr(
-        "marker-start",
-        `url(#${markerId})`
-      );
-
-      updatePagJsonDisplay();
-      console.log(`Arrowtail updated to '${arrowtailType}' for link ${linkId}`);
-    }
-  }
-
-  document
-    .getElementById("arrowtail-normal")
-    .addEventListener("click", () => handleArrowtailClick("normal"));
-  document
-    .getElementById("arrowtail-odot")
-    .addEventListener("click", () => handleArrowtailClick("odot"));
-  document
-    .getElementById("arrowtail-tail")
-    .addEventListener("click", () => handleArrowtailClick("tail"));
-
-  //TODO: Kann man anstatt mit selected Link wie bei delete-link nicht direkt mit linkId arbeiten?
-  document.getElementById("straighten-link").addEventListener("click", () => {
-    const linkMenu = document.getElementById("link-context-menu");
-    const linkId = linkMenu.getAttribute("data-link-id");
-    const selectedLink = jsonData.links.find((link) => link.linkId === linkId);
-    if (selectedLink) {
-      resetLinkCurve(selectedLink);
-      updatePagJsonDisplay();
-    }
-  });
-
-  document
-    .getElementById("toggle-dashed-link")
-    .addEventListener("click", () => {
-      const linkMenu = document.getElementById("link-context-menu");
-      const linkId = linkMenu.getAttribute("data-link-id");
-      const selectedLink = jsonData.links.find(
-        (link) => link.linkId === linkId
-      );
-      if (selectedLink && !selectedLink.isDashed) {
-        setDashed(selectedLink);
-        updatePagJsonDisplay();
-      } else if (selectedLink && selectedLink.isDashed) {
-        unsetDashed(selectedLink);
-        updatePagJsonDisplay();
-      }
-    });
-
-  //wird halt so oft ausgeführt wie oft "visualize" button gedrückt wurde... was macht man dagegen
-  document.getElementById("delete-link").addEventListener("click", () => {
-    const linkMenu = document.getElementById("link-context-menu");
-    const linkId = linkMenu.getAttribute("data-link-id");
-    if (linkId) {
-      deleteLinkJson(linkId);
-      deleteLinkVisualization(linkId);
-      updatePagJsonDisplay();
-      if (linkMenu) {
-        linkMenu.style.display = "none";
-      }
-    }
-  });
-
+  document.getElementById("straighten-link").addEventListener("click", resetLinkCurve);
+  document.getElementById("toggle-dashed-link").addEventListener("click", toggleDashedJson);
+  document.getElementById("delete-link").addEventListener("click", deleteLinkJson);
   setupLinkColorPalette();
 }
 
-function resetLinkCurve(selectedLink) {
-  const sourceNode = selectedLink.source;
-  const targetNode = selectedLink.target;
+// Backend & Frontend
+function updateArrowhead(type){
+  const linkId = getSelectedLinkId();
+  const selectedLink = jsonData.links.find((link) => link.linkId === linkId);
+  updateArrowheadJson(selectedLink, type);
+  updateArrowheadVisualization(selectedLink);
+  updatePagJsonDisplay()
+}
 
-  selectedLink.linkControlX = (sourceNode.x + targetNode.x) / 2;
-  selectedLink.linkControlY = (sourceNode.y + targetNode.y) / 2;
+// Backend: Aktualisiert Arrowhead im jsonData
+function updateArrowheadJson(selectedLink, type) {
+  selectedLink.arrowhead = type;
+}
+
+// Frontend: Aktualisiert die Darstellung des Arrowheads
+function updateArrowheadVisualization(link) {
+  const markerId = `marker-${link.linkId}-end`;
+  setupArrowMarker(d3.select("svg"), markerId, link.arrowhead, link.linkColor, "auto");
+  d3.select(`#link-${link.linkId}`).attr("marker-end", `url(#${markerId})`);
+}
+
+// Backend & Frontend
+function updateArrowtail(type){
+  const linkId = getSelectedLinkId();
+  const selectedLink = jsonData.links.find((link) => link.linkId === linkId);
+  updateArrowtailJson(selectedLink, type);
+  updateArrowtailVisualization(selectedLink);
+  updatePagJsonDisplay()
+}
+
+// Backend: Aktualisiert Arrowtail im jsonData
+function updateArrowtailJson(selectedLink, type) {
+  selectedLink.arrowtail = type;
+}
+
+// Frontend: Aktualisiert die Darstellung des Arrowtails
+function updateArrowtailVisualization(link) {
+  const markerId = `marker-${link.linkId}-start`;
+  setupArrowMarker(d3.select("svg"), markerId, link.arrowtail, link.linkColor, "auto-start-reverse");
+  d3.select(`#link-${link.linkId}`).attr("marker-start", `url(#${markerId})`);
+}
+
+
+// Backend: Schaltet gestrichelte Linien um
+function toggleDashedJson() {
+  const linkId = getSelectedLinkId();
+
+  const selectedLink = jsonData.links.find((link) => link.linkId === linkId);
+
+  selectedLink.isDashed = !selectedLink.isDashed;
+  toggleDashedVisualization(selectedLink);
+  updatePagJsonDisplay();
+}
+
+// Backend: Löscht einen Link
+function deleteLinkJson() {
+  const linkId = getSelectedLinkId();
+  jsonData.links = jsonData.links.filter((link) => link.linkId !== linkId);
+  deleteLinkVisualization(linkId);
+  updatePagJsonDisplay();
+}
+
+// Frontend: Holt die ID des aktuell ausgewählten Links
+function getSelectedLinkId() {
+  const linkMenu = document.getElementById("link-context-menu");
+  return linkMenu ? linkMenu.getAttribute("data-link-id") : null;
+}
+
+
+// Frontend: Schaltet gestrichelte Linie um
+function toggleDashedVisualization(link) {
+  d3.select(`#link-${link.linkId}`).attr("stroke-dasharray", link.isDashed ? "4 2" : null);
+}
+
+// Frontend: Entfernt Link aus der Visualisierung
+function deleteLinkVisualization(linkId) {
+  d3.select(`#link-${linkId}`).remove();
+}
+
+// Frontend: Setzt eine Kurve zurück
+function resetLinkCurve() {
+  const linkId = getSelectedLinkId();
+
+  const selectedLink = jsonData.links.find((link) => link.linkId === linkId);
+
+  selectedLink.linkControlX = (selectedLink.source.x + selectedLink.target.x) / 2;
+  selectedLink.linkControlY = (selectedLink.source.y + selectedLink.target.y) / 2;
   selectedLink.isCurved = false;
 
-  d3.select(`#link-${selectedLink.linkId}`).attr(
-    "d",
-    calculateLinkPath(selectedLink)
-  );
+  d3.select(`#link-${selectedLink.linkId}`).attr("d", calculateLinkPath(selectedLink));
 }
 
-function setDashed(selectedLink) {
-  selectedLink.isDashed = true;
 
-  d3.select(`#link-${selectedLink.linkId}`).attr("stroke-dasharray", "4 2");
-}
-
-function unsetDashed(selectedLink) {
-  selectedLink.isDashed = false;
-
-  d3.select(`#link-${selectedLink.linkId}`).attr("stroke-dasharray", null);
-}
-
-function deleteLinkJson(linkId) {
-  jsonData.links = jsonData.links.filter((link) => link.linkId !== linkId); //löscht link aus jsonData
-}
-
-//Entfernt Links aus dem jsonData und von svg canvas
-function deleteLinkVisualization(linkId) {
-  d3.select(`#link-${linkId}`).remove(); //löscht link von svg canvas
-}
-
+// Frontend: Setzt die Farbpalette für Links
 function setupLinkColorPalette() {
-  const linkColorPalette = document.getElementById("link-color-palette");
-  linkColorPalette.innerHTML = "";
-
+  const palette = document.getElementById("link-color-palette");
+  palette.innerHTML = "";
   allowedColors.forEach((color) => {
-    const colorSwatch = document.createElement("div");
-    colorSwatch.className = "color-swatch";
-    colorSwatch.style.backgroundColor = color;
-
-    colorSwatch.addEventListener("click", () => {
-      const linkMenu = document.getElementById("link-context-menu");
-      const linkId = linkMenu.getAttribute("data-link-id");
-
-      if (linkId) {
-        changeLinkColor(linkId, color);
-      }
-    });
-
-    linkColorPalette.appendChild(colorSwatch);
+    const swatch = document.createElement("div");
+    swatch.className = "color-swatch";
+    swatch.style.backgroundColor = color;
+    swatch.addEventListener("click", () => changeLinkColor(getSelectedLinkId(), color));
+    palette.appendChild(swatch);
   });
 }
 
+// Frontend: Ändert die Farbe eines Links
 function changeLinkColor(linkId, color) {
   const selectedLink = jsonData.links.find((link) => link.linkId === linkId);
 
-  if (selectedLink) {
-    selectedLink.linkColor = color;
-
-    const svgLink = d3.select(`#link-${linkId}`);
-    svgLink.attr("stroke", color);
-
-    if (selectedLink.arrowhead) {
-      const markerId = `marker-${selectedLink.linkId}-end`;
-      setupArrowMarker(
-        d3.select("svg"),
-        markerId,
-        selectedLink.arrowhead,
-        color,
-        "auto"
-      );
-      d3.select(`#link-${selectedLink.linkId}`).attr(
-        "marker-end",
-        `url(#${markerId})`
-      );
-    }
-
-    if (selectedLink.arrowtail) {
-      const markerId = `marker-${selectedLink.linkId}-start`;
-      setupArrowMarker(
-        d3.select("svg"),
-        markerId,
-        selectedLink.arrowtail,
-        color,
-        "auto-start-reverse"
-      );
-      d3.select(`#link-${selectedLink.linkId}`).attr(
-        "marker-start",
-        `url(#${markerId})`
-      );
-    }
-    updatePagJsonDisplay();
-  }
+  selectedLink.linkColor = color;
+  d3.select(`#link-${linkId}`).attr("stroke", color);
+  if (selectedLink.arrowhead) updateArrowheadVisualization(selectedLink);
+  if (selectedLink.arrowtail) updateArrowtailVisualization(selectedLink);
+  updatePagJsonDisplay();
 }
